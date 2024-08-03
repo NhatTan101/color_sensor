df6dca93764d47e69c4941c240d22d4cb619799b
NhatTan101 <nhattanbruh@gmail.com>
2024-08-03 17:22:23 +0700

    Update color_sensor.cpp

--- color_sensor.cpp
+++ color_sensor.cpp
@@ -1,8 +1,11 @@
 #include <Wire.h>
-#include <Adafruit_TCS34725.h>
+#include "Adafruit_TCS34725.h"
 
 // Khởi tạo đối tượng TCS34725
 Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
+
+// Ngưỡng để xác định màu trắng
+const float WHITE_THRESHOLD = 200.0; // Ngưỡng độ sáng (c)
 
 void setup() {
   Serial.begin(9600);
@@ -17,21 +20,16 @@ void setup() {
 
 void loop() {
   uint16_t r, g, b, c;
-  float red, green, blue;
 
   // Đọc giá trị màu sắc từ cảm biến
   tcs.getRawData(&r, &g, &b, &c);
 
-  // Chuyển đổi giá trị raw thành giá trị màu thực tế
-  red = r / (float)c * 255.0;
-  green = g / (float)c * 255.0;
-  blue = b / (float)c * 255.0;
-
-  // Hiển thị giá trị màu sắc
-  Serial.print("Red: "); Serial.print(red); Serial.print(" ");
-  Serial.print("Green: "); Serial.print(green); Serial.print(" ");
-  Serial.print("Blue: "); Serial.print(blue); Serial.print(" ");
-  Serial.print("Clear: "); Serial.print(c); Serial.println(" ");
+  // Kiểm tra xem màu có phải là màu trắng hay không
+  if (c > WHITE_THRESHOLD) {
+    Serial.println("Detected color is White");
+  } else {
+    Serial.println("Detected color is NOT White");
+  }
 
   // Đợi 1 giây trước khi đọc lại
   delay(1000);
